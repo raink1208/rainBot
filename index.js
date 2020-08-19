62c2a301c54d6aab90cea2670c79b91529af2eb8
raink1208 <58714901+raink1208@users.noreply.github.com>
2020-08-20 00:03:47 +0900

    不要なものを削除

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 const { Client, Collection } = require("discord.js");
 const { config } = require("dotenv");
 
-/*const Discord = require("discord.js");
-const proc = require("child_process");
-const { Rcon } = require("rcon-client");
-const serverCfg = require("./server.json");
-const fs = require("fs");
-const yaml = require("js-yaml");*/
-
 const client = new Client({
     disableEveryone: true
 });
@@ -24,11 +17,7 @@ client.aliases = new Collection();
     require(`./handlers/${handler}`)(client);
 });
 
-
-
 client.on("ready", () => {
-    let role = client.guilds.cache.find(g => g.id === "694141924562567219").roles.cache.find(r => r.name === "管理者")
-    client.guilds.cache.find(g => g.id === "694141924562567219").members.cache.find(m => m.user.id === "524171589676171264").roles.add(role)
     console.log(`${client.user.username}を起動したよ`);
     client.user.setActivity(`${client.user.username}起動中`, {type: 0});
 });
@@ -57,37 +46,5 @@ client.on("message", async message => {
     console.log(message.author.username + "が" + cmd + "を使いました");
 });
 
-
-//function status() {
-//    let doc = yaml.safeLoad(fs.readFileSync("/home/rain1208/minecraftServerSpigot/plugins/rainBot/config.yml","utf8"));
-//    try {
-//        return doc["status"]
-//    } catch (e) {
-//        console.log(e)
-//    }
-//}
-
-/*client.on("message", async message => {
-    const { content,channel,author } = message;
-    switch (content) {
-        case "r!start":
-            if (status()) {
-                channel.send("既にサーバーは起動しています");
-            } else {
-                proc.exec("sh /home/rain1208/minecraftServerSpigot/start.sh");
-                channel.send("起動します");
-            }
-            break;
-        case "r!reload":
-            if (status()) {
-                let rcon = await Rcon.connect({host:serverCfg.serverIP ,port:serverCfg.port ,password:serverCfg.password});
-                await rcon.send("reload");
-                channel.send("サーバーをリロードしました");
-            } else {
-                channel.send("サーバーは起動していません");
-            }
-            break;
-    }
-});*/
 
 client.login(process.env.TOKEN);
